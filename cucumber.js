a86241554b67380f9d95bee83b4b637506370d04
Lázaro Martínez <l.santooss@gmail.com>
2023-01-10 16:47:55 -0600

    Update cucumber.js

--- cucumber.js
+++ cucumber.js
@@ -2,6 +2,7 @@ module.exports = {
   default: [
     "--require-module ts-node/register",
     "--require features/**/*.ts",
+    "--format html:cucumber-report.html",
     "--publish-quiet",
   ].join(" "),
 };
